a1324e1044acccfd9f12160f294bef5b359aa08b
Heiner <heineraes@gmail.com>
2015-01-23 21:54:22 -0500

    verificación si existen productos con el mismo nombre

--- models/producto/producto_verificar.php
+++ models/producto/producto_verificar.php
@@ -1,6 +1,6 @@
 <?
 	include "../../models/conexion.php";
-	$usu_per = $_GET['producto'];
+	$producto = $_GET['producto'];
 	$sql = "SELECT * FROM producto WHERE producto = '$producto'";
 	$per = mysql_query($sql);
 	$num_rs_per = mysql_num_rows($per);
